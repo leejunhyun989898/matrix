d970a5d28d505002a37f4777ca790e4d1a2d6639
kd9397 <kd9397@DESKTOP-VK3SSIA>
2023-03-28 00:25:14 +0900

    matrixmain.c(visual-->github)

--- matrix/matrixmain.c
+++ matrix/matrixmain.c
@@ -2,65 +2,83 @@
 #include <stdlib.h>
 
 #define MAX_TERMS 100
+
 typedef struct {
-	int row;
-	int col;
-	int value;
+    int row;
+    int col;
+    int value;
 } element;
 
 typedef struct sparseMatrix {
-	element data[MAX_TERMS];
-	int rows;
-	int cols;
-	int terms;
-}SparseMatrix;
-
-SparseMatrix transpose(SparseMatrix a)
-{
-	SparseMatrix b;
-	
-	int bindex;
-	b.rows = a.cols;
-	b.cols = a.rows;
-
-	b.terms = a.terms;
-
-	if (a.terms > 0) {
-		bindex = 0;
-		for (int c = 0; c < a.cols; c++) {
-			for (int i = 0; i < a.terms; i++) {
-				if (a.data[i].col == c)
-				{
-					b.data[bindex].row = a.data[i].col;
-					b.data[bindex].col = a.data[i].row;
-					b.data[bindex].value = a.data[i].value;
-					bindex++;
-				}
-			}
-		}
-	}
-	return b;
-}
+    element data[MAX_TERMS];
+    int rows;
+    int cols;
+    int terms;
+} SparseMatrix;
 
 void matrix_print(SparseMatrix a)
 {
-	for (int i = 0; i < a.terms; i++)
-	{
-		printf("(%d %d %d)\n", a.data[i].row, a.data[i].col, a.data[i].value);
+    for (int i = 0; i < a.terms; i++)
+    {
+        printf("(%d %d %d)\n", a.data[i].row, a.data[i].col, a.data[i].value);
 
-	}
-	printf("\n");
+    }
+    printf("\n");
+}
+
+SparseMatrix* transpose(SparseMatrix* a)
+{
+    SparseMatrix* b = (SparseMatrix*)malloc(sizeof(SparseMatrix));
+
+    b->rows = a->cols;
+    b->cols = a->rows;
+    b->terms = a->terms;
+
+    if (a->terms > 0) {
+        int bindex = 0;
+        for (int c = 0; c < a->cols; c++) {
+            for (int i = 0; i < a->terms; i++) {
+                if (a->data[i].col == c)
+                {
+                    b->data[bindex].row = a->data[i].col;
+                    b->data[bindex].col = a->data[i].row;
+                    b->data[bindex].value = a->data[i].value;
+                    bindex++;
+                }
+            }
+        }
+    }
+
+    return b;
 }
 
 int main()
 {
-	SparseMatrix m = { {{0,3,7},{1,0,9} ,{1,5,8} ,{3,0,6} ,{3,1,5} ,{4,5,1},{5,2,2}},6,6,7 };
-	SparseMatrix result;
+    SparseMatrix *m = (SparseMatrix*)malloc(sizeof(SparseMatrix));
+    SparseMatrix *result;
 
-	printf("2차원 행렬\n");
-	matrix_print(m);
-	result = transpose(m);
-	printf("전치행렬\n");
-	matrix_print(result);
-	return 0;
+    int rows, cols, terms;
+    printf("행과 열 그리고 항의 개수를 입력해주세요: ");
+    scanf("%d %d %d", &rows, &cols, &terms);
+
+    m->rows = rows;
+    m->cols = cols;
+    m->terms = terms;
+
+    printf("희소행렬의 항의 정보를 입력해주세요(row, col, value):\n");
+    for (int i = 0; i < terms; i++) {
+        scanf("%d %d %d", &m->data[i].row, &m->data[i].col, &m->data[i].value);
+    }
+
+    printf("2차원 행렬\n");
+    matrix_print(*m);
+
+    result = transpose(m);
+    printf("전치행렬\n");
+    matrix_print(*result);
+
+    free(m);
+    free(result);
+
+    return 0;
 }
